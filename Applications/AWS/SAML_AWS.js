7591dd91859fe34236b33fe74574dc1b4f112a4c
Martin <athome@Martins-MacBook-Pro.local>
2016-10-13 23:27:52 +0200

    Amazon Web Services SAML 2.0 script

---
+++ Applications/AWS/SAML_AWS.js
@@ -0,0 +1,30 @@
+setVersion('2');
+setIssuer(Issuer);
+setSubjectName(LoginUser.Username);
+setAudience('urn:amazon:webservices');
+setRecipient(ServiceUrl);
+setSignatureType('Assertion');
+setServiceUrl(ServiceUrl);
+setHttpDestination(ServiceUrl);
+setNameFormat('persistent');
+
+/* Attribute: RoleSessionName */
+setAttribute('https://aws.amazon.com/SAML/Attributes/RoleSessionName', LoginUser.Username);
+
+/* Attribute: Role */
+/* http://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html */
+var roleNames = LoginUser.RoleNames;
+var attrArray = new Array();
+for (var i=0; i < roleNames.Length; i++){
+switch (roleNames[i]){
+    case 'Centrify_Role1':
+        AWSRole = 'arn:aws:iam::account-id:role/role-name,arn:aws:iam::account-id:saml-provider/provider-name';
+		attrArray.push(AWSRole);
+        break;
+    case 'Centriy_Role2':
+        AWSRole = 'arn:aws:iam::account-id:role/role-name,arn:aws:iam::account-id:saml-provider/provider-name';
+		attrArray.push(AWSRole);
+        break;
+	}
+}
+setAttributeArray('https://aws.amazon.com/SAML/Attributes/Role', attrArray);
